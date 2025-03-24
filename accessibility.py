144b2b0a83c3fd1f22461ca73bd5488746f80004
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-03-24 12:45:39 +0000

    Added chrome

--- accessibility.py
+++ accessibility.py
@@ -47,7 +47,7 @@ def usage():
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hufc:", ["help", "url=", 'file='])
+        opts, args = getopt.getopt(sys.argv[1:], "hufc:", ["help", "url=", 'file=', 'chrome'])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
