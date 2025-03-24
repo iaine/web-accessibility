9168e1ca11381a084ae09f0396a33d43bffacb2b
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-03-24 12:50:07 +0000

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='CIM-Access',
+      version='1.0',
+      description='Scripts for simple accessibility',
+      author='Iain Emsley',
+      author_email='iain.emsley@warwick.ac.uk',
+      url='https://warwick.ac.uk/fac/cross_fac/cim/people/iain-emsley',
+      packages=['selenium', 'getopt'],
+     )
