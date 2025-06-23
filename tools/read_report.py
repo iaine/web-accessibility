070adc3b3b063d09bc4253480b2adf002b84effc
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-06-23 12:06:17 +0100

    Tidying up the report space

--- tools/read_report.py
+++ tools/read_report.py
@@ -9,17 +9,17 @@ errors_fh = []
 for report in reports:
     get_urls = report.split("_")
     testbrowse = get_urls[-2:-1][0]
-    url = "/".join(get_urls[:-2]).replace("./reports/","")
+    url = "/".join(get_urls[:-2]).replace("./reports/","").strip()
     with open(report, 'r') as fh:
         data = json.load(fh)
 
     for d in data['incomplete']:
         for node in d['nodes']:
-            errors_fh.append([url, testbrowse,d['description'], d['id'], node['failureSummary'], node['html'], node['impact'], "incomplete" ])
+            errors_fh.append([url, testbrowse,d['description'].strip(), d['id'], node['failureSummary'].strip(), node['html'], node['impact'], "incomplete" ])
 
     for d in data['violations']:
         for node in d['nodes']:
-            errors_fh.append([url, testbrowse,d['description'], d['id'], node['failureSummary'], node['html'], node['impact'], "violations" ])
+            errors_fh.append([url, testbrowse,d['description'].strip(), d['id'], node['failureSummary'].strip(), node['html'], node['impact'], "violations" ])
 
 columns=['url', 'browser', 'description', 'id', 'summary', 'html', 'impact', 'state']
 errors_df = pd.DataFrame(errors_fh,columns=columns)
